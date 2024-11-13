1f604000b346e19e601bdaec7bc0ac8c060c7ca0
Trey Tallent <treydentallent@gmail.com>
2024-11-13 19:38:58 +0800

    Begin migration of css to theme.json

--- functions.php
+++ functions.php
@@ -92,79 +92,6 @@ function block_variations_register()
 }
 add_action('enqueue_block_editor_assets', 'block_variations_register');
 
-// Enqeue block style variation css
-function block_variations_css()
-{
-	wp_enqueue_style('shared-css', get_template_directory_uri() . '/assets/css/shared.css');
-
-	wp_enqueue_block_style(
-		'core/button',
-		array(
-			'handle' => 'block-variations-css-buttons',
-			'src' => get_template_directory_uri() . '/assets/css/buttons.css',
-			'path' => get_template_directory_uri() . '/assets/css/buttons.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'core/group',
-		array(
-			'handle' => 'block-variations-css-groups',
-			'src' => get_template_directory_uri() . '/assets/css/group.css',
-			'path' => get_template_directory_uri() . '/assets/css/group.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'core/image',
-		array(
-			'handle' => 'block-variations-css-images',
-			'src' => get_template_directory_uri() . '/assets/css/image.css',
-			'path' => get_template_directory_uri() . '/assets/css/image.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'core/navigation',
-		array(
-			'handle' => 'block-variations-css-navigation',
-			'src' => get_template_directory_uri() . '/assets/css/navigation.css',
-			'path' => get_template_directory_uri() . '/assets/css/navigation.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'artedwa-blocks/event-card',
-		array(
-			'handle' => 'block-variations-css-cards',
-			'src' => get_template_directory_uri() . '/assets/css/card.css',
-			'path' => get_template_directory_uri() . '/assets/css/card.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'artedwa-blocks/tabs-wrapper',
-		array(
-			'handle' => 'block-variations-tabs-wrapper',
-			'src' => get_template_directory_uri() . '/assets/css/tabs.css',
-			'path' => get_template_directory_uri() . '/assets/css/tabs.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'artedwa-blocks/carousel-wrapper',
-		array(
-			'handle' => 'block-variations-carousel-wrapper',
-			'src' => get_template_directory_uri() . '/assets/css/carousel.css',
-			'path' => get_template_directory_uri() . '/assets/css/carousel.css',
-		)
-	);
-	wp_enqueue_block_style(
-		'artedwa-blocks/scrolling-carousel',
-		array(
-			'handle' => 'block-variations-scrolling-carousel',
-			'src' => get_template_directory_uri() . '/assets/css/scrolling-carousel.css',
-			'path' => get_template_directory_uri() . '/assets/css/scrolling-carousel.css',
-		)
-	);
-}
-add_action('init', 'block_variations_css');
-
-
 // Modifies the query for upcoming_date_query_loop_name on the front-end
 add_filter('pre_render_block', 'upcoming_date_query_loop_name_pre_render_block', 10, 2);
 function upcoming_date_query_loop_name_pre_render_block($pre_render, $parsed_block)
@@ -217,3 +144,96 @@ function past_date_query_loop_name_pre_render_block($pre_render, $parsed_block)
 	}
 	return $pre_render;
 }
+
+// Enqueues global css stylesheets on the front.
+if (! function_exists('enqueue_stylesheet')) :
+
+	function enqueue_stylesheet()
+	{
+		wp_enqueue_style(
+			'style-css',
+			get_parent_theme_file_uri('style.css'),
+		);
+		wp_enqueue_style(
+			'layouts-css',
+			get_parent_theme_file_uri('assets/css/layouts.css')
+		);
+	}
+endif;
+add_action('wp_enqueue_scripts', 'enqueue_stylesheet');
+
+// Enqueues editor-style.css in the editor & block styles.
+if (! function_exists('enqueue_after_theme')) :
+
+	function enqueue_after_theme()
+	{
+		add_editor_style(get_parent_theme_file_uri('assets/css/editor-style.css'));
+		add_editor_style(get_parent_theme_file_uri('assets/css/layouts.css'));
+
+		wp_enqueue_block_style(
+			'core/button',
+			array(
+				'handle' => 'buttons-css',
+				'src' => get_template_directory_uri() . '/assets/css/button.css',
+				'path' => get_template_directory_uri() . '/assets/css/button.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'core/group',
+			array(
+				'handle' => 'block-variations-css-groups',
+				'src' => get_template_directory_uri() . '/assets/css/group.css',
+				'path' => get_template_directory_uri() . '/assets/css/group.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'core/image',
+			array(
+				'handle' => 'block-variations-css-images',
+				'src' => get_template_directory_uri() . '/assets/css/image.css',
+				'path' => get_template_directory_uri() . '/assets/css/image.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'core/navigation',
+			array(
+				'handle' => 'block-variations-css-navigation',
+				'src' => get_template_directory_uri() . '/assets/css/navigation.css',
+				'path' => get_template_directory_uri() . '/assets/css/navigation.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'artedwa-blocks/event-card',
+			array(
+				'handle' => 'block-variations-css-cards',
+				'src' => get_template_directory_uri() . '/assets/css/card.css',
+				'path' => get_template_directory_uri() . '/assets/css/card.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'artedwa-blocks/tabs-wrapper',
+			array(
+				'handle' => 'block-variations-tabs-wrapper',
+				'src' => get_template_directory_uri() . '/assets/css/tabs.css',
+				'path' => get_template_directory_uri() . '/assets/css/tabs.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'artedwa-blocks/carousel-wrapper',
+			array(
+				'handle' => 'block-variations-carousel-wrapper',
+				'src' => get_template_directory_uri() . '/assets/css/carousel.css',
+				'path' => get_template_directory_uri() . '/assets/css/carousel.css',
+			)
+		);
+		wp_enqueue_block_style(
+			'artedwa-blocks/scrolling-carousel',
+			array(
+				'handle' => 'block-variations-scrolling-carousel',
+				'src' => get_template_directory_uri() . '/assets/css/scrolling-carousel.css',
+				'path' => get_template_directory_uri() . '/assets/css/scrolling-carousel.css',
+			)
+		);
+	}
+endif;
+add_action('after_setup_theme', 'enqueue_after_theme');
